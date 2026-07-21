2a608d4b583abf34fd4add370e4cf747871fc7b8
vydough <s4138829@student.rmit.edu.au>
2026-07-21 12:47:46 +1000

    changes to provider.tf
    Removed instructions

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -8,8 +8,4 @@ terraform {
 }
 provider "aws" {
     region = "ap-southeast-2"
-    # Credentials are NOT set here. Terraform will pick them up automatically from,
-    # in order of precedence: environment variables (AWS_ACCESS_KEY_ID / AWS_SECRET_ACCESS_KEY),
-    # your shared credentials file (~/.aws/credentials), or an assumed IAM role.
-    # Never hardcode access_key/secret_key in this file.
 }
